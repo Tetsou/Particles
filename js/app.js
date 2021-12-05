020e7acc770f96ce21bf04395913a20ed7bcb59f
Thomas <thomas.droy@yahoo.co.uk>
2021-12-05 17:36:39 +0000

    Typography styles added to css

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*
+Name:       Particles
+Date:       5th Dec 2021
+Version:    1.0.0
+*/
+// Get the 2d canvas
+const canvas = document.getElementById('src');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = "green";
+ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
